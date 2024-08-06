f152ff07818155f8d872cf533c37294d4a35c7e1
Stlim Browser <137099249+skamen13@users.noreply.github.com>
2024-08-06 19:44:46 +0300

    changed model back

--- main.js
+++ main.js
@@ -84,7 +84,7 @@ async function StartAI(chatt = [], socket, question) {
                 "content": question,
             }
         ],
-        "model": "llama3-groq-70b-8192-tool-use-preview",
+        "model": "llama-3.1-70b-versatile",
         "temperature": 1,
         "max_tokens": 1024,
         "top_p": 0.4,
